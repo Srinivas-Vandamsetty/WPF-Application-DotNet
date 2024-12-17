4b66404c611006a7489fa3a1128d3cc3574643f2
Srinivas Vandamsetty <vandamsetty.saisrinivas@gmail.com>
2024-12-17 18:05:29 +0530

    Add project files.

---
+++ Windows Application/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace WpfApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Get input text from TextBox
+            string inputText = InputTextBox.Text;
+
+            // Display input text in the Label
+            OutputLabel.Content = $"You entered: {inputText}";
+        }
+    }
+}
